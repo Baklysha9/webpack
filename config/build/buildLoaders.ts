5fcdacabe56c7ab25e9b316ba724b422580d5936
Bogdan <bogdancoo@yandex.ru>
2025-11-25 13:05:13 +0300

    start

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,25 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const tsLoader = {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        };
+
+    const cssLoader = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader",
+            ],
+        };
+
+    return [
+        tsLoader,
+        cssLoader
+    ]
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { Counter } from "./components/Counter";
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<div><Counter/></div>)
